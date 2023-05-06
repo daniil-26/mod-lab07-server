9eda4834076a74bd5d66e3dcf7c9713063f6dd60
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-05-06 15:42:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Lab07
+{
+    class Program
+    {
+        static double intakeIntensity = 7;
+        static double serviceIntensity = 5;
+        static int threadsNumber = 3;
+        static int requestsNumber = 1000;
+
+        static void Main()
+        {
+            Statistics statistics = new Statistics();
+            Server server = new Server(threadsNumber, (int)(1000 / serviceIntensity), statistics);
+            Client client = new Client(server, (int)(1000 / intakeIntensity), statistics);
+            client.createRequests(requestsNumber);
+        }
+    }
+}

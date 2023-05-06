7635ccdef575fe324cebca02443913784c23c5fe
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-05-06 15:10:01 +0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Lab07
+{
+    class Client
+    {
+        
+        // public bool bbb = true;
+
+        Server server;
+        Statistics statistics;
+
+        Random rnd = new Random();
+        public int averageTime;
+
+        public delegate void EventHandler<ProcEventArgs>(Client client, ProcEventArgs e);
+        public event EventHandler<ProcEventArgs> request;
+
+        public Client(Server server, int averageTime, Statistics statistics)
+        {
+            this.server = server;
+            this.averageTime = averageTime;
+            this.request += server.proc;
+            this.statistics = statistics;
+        }
+
+        public void createRequests(int n)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(Requests));
+            thread.Start(n);
+        }
+
+        private void Requests(object obj)
+        {
+            if (obj is int n)
+            {
+                for (int id = 0; id < n; id++)
+                {
+                    OnProc(new ProcEventArgs(id));
+                    Thread.Sleep(rnd.Next(2 * averageTime));
+                    // Thread.Sleep(averageTime);
+                }
+                // bbb = false;
+                statistics.total("results.txt", this, server);
+            }
+        }
+
+        protected virtual void OnProc(ProcEventArgs e)
+        {
+            EventHandler<ProcEventArgs> handler = request;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Lab07
+{
+    struct PoolRecord
+    {
+        public Thread thread;
+        public bool inUse;
+    }
+
+    class Server
+    {
+        static int threadsNumber;
+        static int averageTime;
+        static bool consoleOutput;
+
+        Random rnd = new Random();
+
+        PoolRecord[] pool;
+        object threadLock = new object();
+
+        Statistics statistics;
+        public int requestCount = 0, processedCount = 0, rejectedCount = 0;
+
+        public Server(int n, int t, Statistics statistics, bool o = false)
+        {
+            threadsNumber = n;
+            averageTime = t;
+            this.statistics = statistics;
+            consoleOutput = o;
+            pool = new PoolRecord[threadsNumber];
+        }
+
+        void Answer(object obj)
+        {
+            if (obj is ProcEventArgs e)
+            {
+                // Thread.Sleep(sleepTime);
+                Thread.Sleep(rnd.Next(2 * averageTime));
+                // Thread.Sleep(averageTime);
+                if (consoleOutput)
+                {
+                    Console.WriteLine(" V  заявка {0} выполнена", e.id);
+                }                
+                pool[e.n].inUse = false;
+            }
+        }
+
+        public void proc(object sender, ProcEventArgs e)
+        {
+            lock (threadLock)
+            {
+                statistics.dimension(pool);
+                if (consoleOutput)
+                {
+                    Console.WriteLine("Заявка с номером: {0}", e.id);
+                }
+                requestCount++;
+                for (int i = 0; i < threadsNumber; i++)
+                {
+                    if (!pool[i].inUse)
+                    {
+                        e.n = i;
+                        pool[i].inUse = true;
+                        pool[i].thread = new Thread(new ParameterizedThreadStart(Answer));
+                        pool[i].thread.Start(e);
+                        if (consoleOutput)
+                        {
+                            Console.WriteLine("Заявка {0} выполняется в потоке {1}", e.id, e.n);
+                        }
+                        processedCount++;
+                        return;
+                    }
+                }
+                if (consoleOutput)
+                {
+                    Console.WriteLine(" X  заявка {0} отклонена", e.id);
+                }
+                rejectedCount++;
+            }
+        }
+    }
+}
